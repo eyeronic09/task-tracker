1e3785014f39365b551b651a6305c16481960e76
aryan gupta <guptaaryan92136@gmail.com>
2025-01-26 22:35:26 +0530

    add the uptade task

--- app.py
+++ app.py
@@ -13,13 +13,34 @@ def loadTasks():
         except json.JSONDecodeError:
             return []
 
+def updateTask(task_id , new_name):
+    tasks = loadTasks()
+    for task in tasks:
+        if task["id"] == task_id:
+            task["name"] = new_name
+            saveTasktoJson(task)
+            print(f"Task ID {task_id} updated successfully to '{new_name}'.")
+            return
+    print(f"Task ID not {task_id} found")
+            
+
+
+'''
+yea so it generate unique id for each task and also check pervisos task id not generate the same 
+'''
 def get_id(tasks):
     return max((task["id"] for task in tasks), default=0) + 1
 
+'''
+open the file and add all the content , pretty self explainatory
+'''
 def saveTasktoJson(tasks):
     with open(file_path, "w") as file:
         json.dump(tasks, file, indent=4)
 
+'''
+add task with unique id and load task form the fuction 
+'''
 def addTask(tasknames):
 
     tasks = loadTasks()
@@ -31,10 +52,3 @@ def addTask(tasknames):
     saveTasktoJson(tasks)
 
 
-
-if len(sys.argv) > 2 and sys.argv[1] == "add":
-    new_task_name = " ".join(sys.argv[2:])
-    addTask(new_task_name)
-    print(f"Task '{new_task_name}' added successfully.")
-else:
-    print("Usage: python app.py add <task_name>")
